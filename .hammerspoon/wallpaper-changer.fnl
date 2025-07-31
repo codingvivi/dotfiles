da14d3bda360572352321e7fec40c9ceeb38d32d
vivi <135711465+codingvivi@users.noreply.github.com>
2025-07-31 17:55:13 +0200

    Hammerspoon: Fix timer dying after like 6 images, clean up

--- .hammerspoon/wallpaper-changer.fnl
+++ .hammerspoon/wallpaper-changer.fnl
@@ -2,16 +2,20 @@
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
 ;                                  requires                                    ;
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
-(local natcmp (require :string.natcmp)) ;; utf8 sorter
+(local natcmp (require :string.natcmp))
+
+;; utf8 sorter
 
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
 ;                                    locals                                    ;
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ user settings ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
 (local WALLPAPER-FOLDER
        (.. (os.getenv :HOME) "/Pictures/Wallpaper Rotation/master"))
 
 (local WALLPAPER-DUR-MINS 30)
 
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ script settings ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
 (local SUPPORTED-FILETYPE-UTIS ["com.apple.pict"
                                 "com.compuserve.gif"
                                 "com.microsoft.bmp"
@@ -21,15 +25,21 @@
                                 "public.png"
                                 "public.tiff"])
 
-(local PAUSE_EVENTS {hs.caffeinate.watcher.screensDidSleep true
-                     hs.caffeinate.watcher.screensaverDidStart true})
-(local RESUME_EVENTS {hs.caffeinate.watcher.screensDidWake true
-                      hs.caffeinate.watcher.screensaverDidStop true})
+(local PAUSE_EVENTS
+       {hs.caffeinate.watcher.screensDidSleep true
+        hs.caffeinate.watcher.screensaverDidStart true})
 
-;(print "UTIs supported:")
-;(each [_ UTI (ipairs SUPPORTED-FILETYPE-UTIS)]
-;  (print UTI))
+(local RESUME_EVENTS
+       {hs.caffeinate.watcher.screensDidWake true
+        hs.caffeinate.watcher.screensaverDidStop true})
 
+; (hs.settings.set "wallpaper-index" 500) ; for debugging
+
+; ~~~~~~~~~~~~~~~~~~~ garbage collection timer protection ~~~~~~~~~~~~~~~~~~~~ ;
+; Global = not garbage collected, empty table = assignable
+(set _G.M {})
+; local alias
+(local M _G.M)
 
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
 ;                                   helpers                                    ;
@@ -45,23 +55,14 @@
     (print (.. "Number of directories: " dirnum))
     (values files filenum dirnum)))
 
-(λ print-file-list [file-list]
-  "Print file list (for debug purposes)"
-  (each [_ file (ipairs file-list)]
-    (print file)))
-
-;; debugging
-;(let [(file-list b c) (get-files WALLPAPER-FOLDER)]
-;  (print-file-list file-list))
-
 (λ hassupportedUTI? [UTI-list path]
   "Checks if file path matches one of the UTIs"
   (each [_ UTI (ipairs UTI-list)]
     (when (= UTI (hs.fs.fileUTI path))
-      (lua "return true")))
+      (lua "return true"))) ; Use the Lua return to exit immediately
   false)
 
-; Explicitly return false if no match
+;; debugging
 
 (λ get-supported-file-paths [UTIs file-paths]
   "Takes a list of files, returns a list of files that match a list of UTIs"
@@ -75,34 +76,32 @@
   "Calculates the next valid index for the file-list using 'case true'.
    Handles wrap-around and initialization from 0 or invalid index.
    Returns nil if file-list is empty or nil."
-  (if (= file-list nil)
-      (print "Error: file list is nil...")
-      (let [image-list-length (length file-list)]
-        ;; (length nil) or (length {}) will be 0
-        (case true
-          ;; Condition 1: No files available
-          (where _ (= 0 image-list-length))
-          (do
-            (print "Error: No images found in file-list.")
-            nil) ; No files, no valid index
-          ;; Condition 2: Invalid or uninitialized current index
-          (where _ (or (not current-idx) (<= current-idx 0)
-                       (> current-idx image-list-length)))
-          (do
-            (print (.. "Info: Invalid or uninitialized index ("
-                       (tostring current-idx) "). Setting to 1."))
-            1) ; Set to first image
-          ;; Condition 3: Reached the end of the list
-          (where _ (= current-idx image-list-length))
-          (do
-            (print "Info: End of file list. Starting over.")
-            1) ; Wrap to first image
-          ;; Default Case: Valid index, not at end of list
-          _
-          (do
-            (print (.. "Info: Advancing index from " current-idx " to "
-                       (+ current-idx 1)))
-            (+ current-idx 1))))))
+  (let [image-list-length (length file-list)]
+    ;; (length nil) or (length {}) will be 0
+    (case true
+      ;; Condition 1: No files available
+      (where _ (= 0 image-list-length))
+      (do
+        (print "Error: No images found in file-list.")
+        nil) ; No files, no valid index
+      ;; Condition 2: Invalid or uninitialized current index
+      (where _ (or (not current-idx) (<= current-idx 0)
+                   (> current-idx image-list-length)))
+      (do
+        (print (.. "Info: Invalid or uninitialized index ("
+                   (tostring current-idx) "). Setting to 1."))
+        1) ; Set to first image
+      ;; Condition 3: Reached the end of the list
+      (where _ (= current-idx image-list-length))
+      (do
+        (print "Info: End of file list. Starting over.")
+        1) ; Wrap to first image
+      ;; Default Case: Valid index, not at end of list
+      _
+      (do
+        (print (.. "Info: Advancing index from " current-idx " to "
+                   (+ current-idx 1)))
+        (+ current-idx 1)))))
 
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ screen stuff ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
 (fn get-available-screens []
@@ -140,8 +139,10 @@
   0.5)
 
 (λ run-rotator [folder]
+  (print "Starting wallpaper change routine...")
   (let [(cadidate-paths cadidate-num cadidate-dir-num) (get-files folder)
-        image-paths (get-supported-file-paths SUPPORTED-FILETYPE-UTIS cadidate-paths)
+        image-paths (get-supported-file-paths SUPPORTED-FILETYPE-UTIS
+                                              cadidate-paths)
         screens (get-available-screens)
         main-screen (hs.screen.mainScreen)]
     (table.sort image-paths natcmp.utf8.lt)
@@ -154,13 +155,19 @@
           (print "Main screen:")
           (print-screen-details main-screen) ; Run changer: get file index...
           (let [next-index (calculate-next-index image-paths
-                                                 (hs.settings.get "wallpaper-index"))
-                next-image (. image-paths next-index)]
+                                                 (hs.settings.get "wallpaper-index"))]
             ;... and change wallpapers
-            (when next-index
-              (hs.settings.set "wallpaper-index" next-index)
-              (iterate-screens screens change-wallpaper-on-screen
-                               (. image-paths next-index))))))))
+            (if (not next-index)
+                (print "Error: Next index position is nil....")
+                (let [next-image (. image-paths next-index)]
+                  (if (not next-image)
+                      (print "Error: next image is nil...")
+                      (do
+                        (hs.settings.set "wallpaper-index" next-index)
+                        (print (.. "Setting next image to " next-image))
+                        (iterate-screens screens change-wallpaper-on-screen
+                                         (. image-paths next-index))
+                        (print "Changed wallpaper successfully!"))))))))))
 
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ timer helpers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
 (fn check-value-change [value-new value-old]
@@ -198,33 +205,35 @@
 (init-WALLPAPER-DUR-MINS WALLPAPER-DUR-MINS)
 
 ;; create timer object that will change wallpaper after certain amount of time
-(local wallpaper-timer
-       (hs.timer.new (hs.settings.get "wallpaper-timer-dur")
-                     (fn [] (run-rotator WALLPAPER-FOLDER))))
+(set M.wallpaper-timer
+     (hs.timer.new (* 60 WALLPAPER-DUR-MINS)
+                   (fn [] (run-rotator WALLPAPER-FOLDER))))
 
 (print "starting wallpaper-timer")
-(wallpaper-timer:start)
+(M.wallpaper-timer:start)
 
 ; --- timer-pausing stuff ---
 (fn pause-wallpaper-timer []
   "gets current wallpaper-timer time, 
 	writes it to persistent var,
 	pauses"
-  (if (not (wallpaper-timer:running))
+  (if (not (M.wallpaper-timer:running))
       (print "Warning: Timer not running. No action taken")
-      (do 
+      (do
         (print "Saving time and pausing")
-        (let [remaining-time (wallpaper-timer:nextTrigger)]
-          (print (.. "Saving remaining wallpaper time: " remaining-time " seconds"))
+        (let [remaining-time (M.wallpaper-timer:nextTrigger)]
+          (print (.. "Saving remaining wallpaper time: " remaining-time
+                     " seconds"))
           (hs.settings.set "remaining-wallpaper-time" remaining-time)
-          (print (.. "Saved " (hs.settings.get "remaining-wallpaper-time") " seconds"))
-          (wallpaper-timer:stop)
+          (print (.. "Saved " (hs.settings.get "remaining-wallpaper-time")
+                     " seconds"))
+          (M.wallpaper-timer:stop)
           (print "Stopped wallpaper-timer")))))
 
 (fn resume-wallpaper-timer []
   "gets saved wallpaper-timer time, 
 	continues timer"
-  (if (wallpaper-timer:running)
+  (if (M.wallpaper-timer:running)
       (print "Warning: Resumer called, but timer was already running. No action taken.")
       (do
         (print "Timer was stopped, running resumer...")
@@ -232,15 +241,15 @@
           (if (not (and remaining-time (> remaining-time 0)))
               (do
                 (print "Warning: No valid remaining time found")
-                (wallpaper-timer:start)
+                (M.wallpaper-timer:start)
                 (print "Restarted wallpaper timer"))
               (do
                 (print "Remaining time found")
-                (wallpaper-timer:setNextTrigger remaining-time)
+                (M.wallpaper-timer:setNextTrigger remaining-time)
                 (print (.. "Recalled remaining time of " remaining-time "s"))
-                (wallpaper-timer:start)
-                (print (.. "Resumed timer with " (hs.settings.get "remaining-wallpaper-time")))))))))
-                
+                (M.wallpaper-timer:start)
+                (print (.. "Resumed timer with "
+                           (hs.settings.get "remaining-wallpaper-time")))))))))
 
 ; pause on screen off var
 (hs.settings.set "remaining-wallpaper-time" 0)
@@ -248,7 +257,6 @@
 (local screen-state-watcher
        (hs.caffeinate.watcher.new (fn [event-type]
                                     (when (. PAUSE_EVENTS event-type)
-                                      (resume-wallpaper-timer)
                                       (print "Screens went to sleep! running pauser")
                                       (pause-wallpaper-timer))
                                     (when (. RESUME_EVENTS event-type)
--- .hammerspoon/wallpaper-changer.lua
+++ .hammerspoon/wallpaper-changer.lua
@@ -1,4 +1,4 @@
--- :fennel:1753909506
+-- :fennel:1753977223
 print("~~~~~~~~~~~~~~~ wallpaper rotator ~~~~~~~~~~~~~~~")
 local natcmp = require("string.natcmp")
 local WALLPAPER_FOLDER = (os.getenv("HOME") .. "/Pictures/Wallpaper Rotation/master")
@@ -6,8 +6,10 @@ local WALLPAPER_DUR_MINS = 30
 local SUPPORTED_FILETYPE_UTIS = {"com.apple.pict", "com.compuserve.gif", "com.microsoft.bmp", "public.heic", "public.heif", "public.jpeg", "public.png", "public.tiff"}
 local PAUSE_EVENTS = {[hs.caffeinate.watcher.screensDidSleep] = true, [hs.caffeinate.watcher.screensaverDidStart] = true}
 local RESUME_EVENTS = {[hs.caffeinate.watcher.screensDidWake] = true, [hs.caffeinate.watcher.screensaverDidStop] = true}
+_G.M = {}
+local M = _G.M
 local function get_files(folder_path)
-  _G.assert((nil ~= folder_path), "Missing argument folder-path on .hammerspoon/wallpaper-changer.fnl:38")
+  _G.assert((nil ~= folder_path), "Missing argument folder-path on .hammerspoon/wallpaper-changer.fnl:48")
   local files, filenum, dirnum = hs.fs.fileListForPath(folder_path, {subdirs = true})
   print(("Folder: " .. folder_path))
   print(#files)
@@ -15,16 +17,9 @@ local function get_files(folder_path)
   print(("Number of directories: " .. dirnum))
   return files, filenum, dirnum
 end
-local function print_file_list(file_list)
-  _G.assert((nil ~= file_list), "Missing argument file-list on .hammerspoon/wallpaper-changer.fnl:48")
-  for _, file in ipairs(file_list) do
-    print(file)
-  end
-  return nil
-end
 local function hassupportedUTI_3f(UTI_list, path)
-  _G.assert((nil ~= path), "Missing argument path on .hammerspoon/wallpaper-changer.fnl:57")
-  _G.assert((nil ~= UTI_list), "Missing argument UTI-list on .hammerspoon/wallpaper-changer.fnl:57")
+  _G.assert((nil ~= path), "Missing argument path on .hammerspoon/wallpaper-changer.fnl:58")
+  _G.assert((nil ~= UTI_list), "Missing argument UTI-list on .hammerspoon/wallpaper-changer.fnl:58")
   for _, UTI in ipairs(UTI_list) do
     if (UTI == hs.fs.fileUTI(path)) then
       return true
@@ -34,8 +29,8 @@ local function hassupportedUTI_3f(UTI_list, path)
   return false
 end
 local function get_supported_file_paths(UTIs, file_paths)
-  _G.assert((nil ~= file_paths), "Missing argument file-paths on .hammerspoon/wallpaper-changer.fnl:66")
-  _G.assert((nil ~= UTIs), "Missing argument UTIs on .hammerspoon/wallpaper-changer.fnl:66")
+  _G.assert((nil ~= file_paths), "Missing argument file-paths on .hammerspoon/wallpaper-changer.fnl:67")
+  _G.assert((nil ~= UTIs), "Missing argument UTIs on .hammerspoon/wallpaper-changer.fnl:67")
   local file_list = {}
   for _, path in ipairs(file_paths) do
     if hassupportedUTI_3f(UTIs, path) then
@@ -46,47 +41,43 @@ local function get_supported_file_paths(UTIs, file_paths)
   return file_list
 end
 local function calculate_next_index(file_list, current_idx)
-  _G.assert((nil ~= current_idx), "Missing argument current-idx on .hammerspoon/wallpaper-changer.fnl:74")
-  _G.assert((nil ~= file_list), "Missing argument file-list on .hammerspoon/wallpaper-changer.fnl:74")
-  if (file_list == nil) then
-    return print("Error: file list is nil...")
+  _G.assert((nil ~= current_idx), "Missing argument current-idx on .hammerspoon/wallpaper-changer.fnl:75")
+  _G.assert((nil ~= file_list), "Missing argument file-list on .hammerspoon/wallpaper-changer.fnl:75")
+  local image_list_length = #file_list
+  local _3_ = true
+  local and_4_ = true
+  if and_4_ then
+    local _ = _3_
+    and_4_ = (0 == image_list_length)
+  end
+  if and_4_ then
+    local _ = _3_
+    print("Error: No images found in file-list.")
+    return nil
   else
-    local image_list_length = #file_list
-    local _3_ = true
-    local and_4_ = true
-    if and_4_ then
+    local and_6_ = true
+    if and_6_ then
       local _ = _3_
-      and_4_ = (0 == image_list_length)
+      and_6_ = (not current_idx or (current_idx <= 0) or (current_idx > image_list_length))
     end
-    if and_4_ then
+    if and_6_ then
       local _ = _3_
-      print("Error: No images found in file-list.")
-      return nil
+      print(("Info: Invalid or uninitialized index (" .. tostring(current_idx) .. "). Setting to 1."))
+      return 1
     else
-      local and_6_ = true
-      if and_6_ then
+      local and_8_ = true
+      if and_8_ then
         local _ = _3_
-        and_6_ = (not current_idx or (current_idx <= 0) or (current_idx > image_list_length))
+        and_8_ = (current_idx == image_list_length)
       end
-      if and_6_ then
+      if and_8_ then
         local _ = _3_
-        print(("Info: Invalid or uninitialized index (" .. tostring(current_idx) .. "). Setting to 1."))
+        print("Info: End of file list. Starting over.")
         return 1
       else
-        local and_8_ = true
-        if and_8_ then
-          local _ = _3_
-          and_8_ = (current_idx == image_list_length)
-        end
-        if and_8_ then
-          local _ = _3_
-          print("Info: End of file list. Starting over.")
-          return 1
-        else
-          local _ = _3_
-          print(("Info: Advancing index from " .. current_idx .. " to " .. (current_idx + 1)))
-          return (current_idx + 1)
-        end
+        local _ = _3_
+        print(("Info: Advancing index from " .. current_idx .. " to " .. (current_idx + 1)))
+        return (current_idx + 1)
       end
     end
   end
@@ -104,25 +95,25 @@ local function get_available_screens()
   end
 end
 local function print_screen_details(screen)
-  _G.assert((nil ~= screen), "Missing argument screen on .hammerspoon/wallpaper-changer.fnl:122")
-  local _13_
+  _G.assert((nil ~= screen), "Missing argument screen on .hammerspoon/wallpaper-changer.fnl:121")
+  local _12_
   do
     local frame = screen:frame()
-    _13_ = (frame.w .. " x " .. frame.h .. "px")
+    _12_ = (frame.w .. " x " .. frame.h .. "px")
   end
-  return print(("Screen " .. screen:name() .. " - " .. _13_))
+  return print(("Screen " .. screen:name() .. " - " .. _12_))
 end
 local function iterate_screens(screen_list, func, _3fargs)
-  _G.assert((nil ~= func), "Missing argument func on .hammerspoon/wallpaper-changer.fnl:128")
-  _G.assert((nil ~= screen_list), "Missing argument screen-list on .hammerspoon/wallpaper-changer.fnl:128")
+  _G.assert((nil ~= func), "Missing argument func on .hammerspoon/wallpaper-changer.fnl:127")
+  _G.assert((nil ~= screen_list), "Missing argument screen-list on .hammerspoon/wallpaper-changer.fnl:127")
   for i, screen in ipairs(screen_list) do
     func(screen, _3fargs)
   end
   return nil
 end
 local function change_wallpaper_on_screen(screen, file_path)
-  _G.assert((nil ~= file_path), "Missing argument file-path on .hammerspoon/wallpaper-changer.fnl:134")
-  _G.assert((nil ~= screen), "Missing argument screen on .hammerspoon/wallpaper-changer.fnl:134")
+  _G.assert((nil ~= file_path), "Missing argument file-path on .hammerspoon/wallpaper-changer.fnl:133")
+  _G.assert((nil ~= screen), "Missing argument screen on .hammerspoon/wallpaper-changer.fnl:133")
   do
     local url = ("file://" .. file_path)
     print(("Setting wallpaper to: " .. url))
@@ -132,7 +123,8 @@ local function change_wallpaper_on_screen(screen, file_path)
   return 0.5
 end
 local function run_rotator(folder)
-  _G.assert((nil ~= folder), "Missing argument folder on .hammerspoon/wallpaper-changer.fnl:142")
+  _G.assert((nil ~= folder), "Missing argument folder on .hammerspoon/wallpaper-changer.fnl:141")
+  print("Starting wallpaper change routine...")
   local cadidate_paths, cadidate_num, cadidate_dir_num = get_files(folder)
   local image_paths = get_supported_file_paths(SUPPORTED_FILETYPE_UTIS, cadidate_paths)
   local screens = get_available_screens()
@@ -146,12 +138,18 @@ local function run_rotator(folder)
     print("Main screen:")
     print_screen_details(main_screen)
     local next_index = calculate_next_index(image_paths, hs.settings.get("wallpaper-index"))
-    local next_image = image_paths[next_index]
-    if next_index then
-      hs.settings.set("wallpaper-index", next_index)
-      return iterate_screens(screens, change_wallpaper_on_screen, image_paths[next_index])
+    if not next_index then
+      return print("Error: Next index position is nil....")
     else
-      return nil
+      local next_image = image_paths[next_index]
+      if not next_image then
+        return print("Error: next image is nil...")
+      else
+        hs.settings.set("wallpaper-index", next_index)
+        print(("Setting next image to " .. next_image))
+        iterate_screens(screens, change_wallpaper_on_screen, image_paths[next_index])
+        return print("Changed wallpaper successfully!")
+      end
     end
   end
 end
@@ -165,7 +163,7 @@ local function check_value_change(value_new, value_old)
   return change
 end
 local function init_WALLPAPER_DUR_MINS(duration)
-  _G.assert((nil ~= duration), "Missing argument duration on .hammerspoon/wallpaper-changer.fnl:177")
+  _G.assert((nil ~= duration), "Missing argument duration on .hammerspoon/wallpaper-changer.fnl:184")
   local duration_secs_new = hs.timer.minutes(duration)
   print(("Converted duration of " .. duration .. " minutes into " .. duration_secs_new .. " seconds"))
   local duration_secs_old = hs.settings.get("wallpaper-timer-dur")
@@ -185,41 +183,40 @@ local function init_WALLPAPER_DUR_MINS(duration)
 end
 print("Initalizing duration and timer")
 init_WALLPAPER_DUR_MINS(WALLPAPER_DUR_MINS)
-local wallpaper_timer
 local function _20_()
   return run_rotator(WALLPAPER_FOLDER)
 end
-wallpaper_timer = hs.timer.new(hs.settings.get("wallpaper-timer-dur"), _20_)
+M["wallpaper-timer"] = hs.timer.new((60 * WALLPAPER_DUR_MINS), _20_)
 print("starting wallpaper-timer")
-wallpaper_timer:start()
+M["wallpaper-timer"]:start()
 local function pause_wallpaper_timer()
-  if not wallpaper_timer:running() then
+  if not M["wallpaper-timer"]:running() then
     return print("Warning: Timer not running. No action taken")
   else
     print("Saving time and pausing")
-    local remaining_time = wallpaper_timer:nextTrigger()
+    local remaining_time = M["wallpaper-timer"]:nextTrigger()
     print(("Saving remaining wallpaper time: " .. remaining_time .. " seconds"))
     hs.settings.set("remaining-wallpaper-time", remaining_time)
     print(("Saved " .. hs.settings.get("remaining-wallpaper-time") .. " seconds"))
-    wallpaper_timer:stop()
+    M["wallpaper-timer"]:stop()
     return print("Stopped wallpaper-timer")
   end
 end
 local function resume_wallpaper_timer()
-  if wallpaper_timer:running() then
+  if M["wallpaper-timer"]:running() then
     return print("Warning: Resumer called, but timer was already running. No action taken.")
   else
     print("Timer was stopped, running resumer...")
     local remaining_time = hs.settings.get("remaining-wallpaper-time")
     if not (remaining_time and (remaining_time > 0)) then
       print("Warning: No valid remaining time found")
-      wallpaper_timer:start()
+      M["wallpaper-timer"]:start()
       return print("Restarted wallpaper timer")
     else
       print("Remaining time found")
-      wallpaper_timer:setNextTrigger(remaining_time)
+      M["wallpaper-timer"]:setNextTrigger(remaining_time)
       print(("Recalled remaining time of " .. remaining_time .. "s"))
-      wallpaper_timer:start()
+      M["wallpaper-timer"]:start()
       return print(("Resumed timer with " .. hs.settings.get("remaining-wallpaper-time")))
     end
   end
@@ -229,7 +226,6 @@ print("Initalizing screen-state watcher")
 local screen_state_watcher
 local function _24_(event_type)
   if PAUSE_EVENTS[event_type] then
-    resume_wallpaper_timer()
     print("Screens went to sleep! running pauser")
     pause_wallpaper_timer()
   else
