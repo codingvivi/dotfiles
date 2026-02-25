3a0298ec4d8d839a9101049d03b03c138220e966
vivi <135711465+codingvivi@users.noreply.github.com>
2026-02-26 00:56:17 +0100

    WIP: add some new views

--- dot_config/emacs/lisp/vv-org-agenda.el
+++ dot_config/emacs/lisp/vv-org-agenda.el
@@ -14,12 +14,29 @@
 
         ("c" "Chores"
          ((tags-todo "domestic")
-          (tags-todo "ricefields"))
+          (tags-todo "+chores+ricefields"))
+         ((org-agenda-sorting-strategy '(priority-down))))
+
+        ("r" "r&r"
+         ((tags-todo "rnr|fun")
+          (tags-todo "+rnr+ricefields"))
+         ((org-agenda-sorting-strategy '(priority-down))))
+
+        ("i" "r&r"
+         ((tags-todo "rnr|fun")
+          (tags-todo "+rnr+ricefields"))
+         ((org-agenda-sorting-strategy '(priority-down))))
+
+        ("t" "Tracks"
+         ((tags-todo "get|recdigging")
+          (tags-todo "dig|recgetting"))
          ((org-agenda-sorting-strategy '(priority-down))))
 
         ("g" "girlfriemd....."
          ((tags-todo "+lucy+berlin")
           (tags-todo "+lucy+home")
+          (tags-todo "+lucy+watch")
+          (tags-todo "+lucy+stuttgart")
           ;; (tags-todo "musicwork-{DJing\\|production\\|st}")
           (tags-todo "lucy-berlin-home"))
          ((org-agenda-sorting-strategy '(priority-down))))))
@@ -30,5 +47,7 @@
     (with-temp-buffer
       (org-agenda-list nil nil 'year) ;; Pre-builds the year view
       (message "Yearly agenda pre-cached."))))
+
+(use-package org-super-agenda)
 
 (provide 'vv-org-agenda)
